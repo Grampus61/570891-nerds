c3f0c2e9b70a6de56358001be70ffbc66d87c8e9
Grampus61 <33198527+Grampus61@users.noreply.github.com>
2017-12-07 15:27:38 +0300

    Предфинальная проверка с исправленным JS

--- js/popup.js
+++ js/popup.js
@@ -1,31 +1,18 @@
 
-var link = document.querySelector(".button-contacts");
+var button = document.querySelector(".button-contacts");
 var popup = document.querySelector(".modal-letter");
-var close = document.querySelector(".modal-close");
+var close = popup.querySelector(".modal-close");
 var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
-var post = popup.querySelector("[name=post]");
 
-link.addEventListener("click", function (evt) {
+button.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-    name.focus();
   });
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
-    form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
   });
-
-  form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    if (!name.value || !post.value) {
+form.addEventListener("submit", function (evt) {
       evt.preventDefault();
-      console.log("Нужно ввести имя и e-mail");
-    } else {
-    localStorage.setItem("name", name.value);
-  }
-    }
-  });
-  
+      console.log("Отправить форму");
+    });
